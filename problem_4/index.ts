b45262f4dbe6790dfca76733184ae1370ab4ab2b
dinh dat <dinhquocdat1310@gmail.com>
2024-04-30 10:02:12 +0700

    Complete problem 4

---
+++ problem_4/index.ts
@@ -0,0 +1,40 @@
+// Requirement:
+// Provide 3 unique implementations of the following function in TypeScript.
+// Comment on the complexity or efficiency of each function.
+// Input: n - any integer
+// Output: return - summation to n, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
+
+//Complexity: O(N) - Efficiency: Less efficient because it run time increases with N
+const firstWaySumToN = (n: number): number => {
+	if (!checkValidNumber(n)) return -1;
+	let sum = 0;
+	for (let index = 1; index <= n; index++) {
+		sum += index;
+	}
+	return sum;
+};
+
+//Complexity: O(1) - Efficiency: Is the best because it calculates the sum in constant time
+const secondWaySumToN = (n: number): number => {
+	if (!checkValidNumber(n)) return -1;
+	return (n * (n + 1)) / 2;
+};
+
+//Complexity: O(N) - Efficiency: Less efficient because uses more memory due to recursion
+const thirdWaySumToN = (n: number): number => {
+	if (!checkValidNumber(n)) return -1;
+	return n === 1 ? 1 : n + thirdWaySumToN(n - 1);
+};
+
+//Check valid number
+const checkValidNumber = (n: number): boolean => {
+	if (!n || n < 0 || n % 1 !== 0) return false;
+	return true;
+};
+
+const resultFirstWaySumToN: number = firstWaySumToN(5);
+const resultSecondWaySumToN: number = secondWaySumToN(5);
+const resultThirdWaySumToN: number = thirdWaySumToN(5);
+console.log(resultFirstWaySumToN);
+console.log(resultSecondWaySumToN);
+console.log(resultThirdWaySumToN);
